5ee60f4bca9acef9009faa752f779eedfb699c0a
amit-hazra <amithazra@wekancode.com>
2023-02-16 15:43:20 +0530

    feat: code migration

--- functions/SearchUsers.js
+++ functions/SearchUsers.js
@@ -1,56 +1,38 @@
-/**
- * Get the list of users either by user email or search text(email, phone, last name).
- * Use Facilitron_common.user collection to fetch user related information
- * @param {userEmail?: string, searchText?: string} payload
- * @returns [users]
- */
+exports = async function(arg){
+  // This default function will get a value and find a document in MongoDB
+  // To see plenty more examples of what you can do with functions see: 
+  // https://www.mongodb.com/docs/atlas/app-services/functions/
 
-function SearchUsers(payload, pagination) {
-  let users = [];
+  // Find the name of the MongoDB service you want to use (see "Linked Data Sources" tab)
+  var serviceName = "mongodb_atlas";
 
-  if (!payload || payload === null) {
-    throw new Error("Invalid payload");
-  }
-  const CALLER_TYPES = context.values.get("caller_types");
+  // Update these to reflect your db/collection
+  var dbName = "db_name";
+  var collName = "coll_name";
 
-  const {
-    userEmail,
-    userId,
-    searchText,
-    callerType = CALLER_TYPES.RENTER,
-  } = payload;
-  if (!userEmail && !userId && !searchText) {
-    throw new Error("Either of one argument is mandatory.");
-  }
+  // Get a collection from the context
+  var collection = context.services.get(serviceName).db(dbName).collection(collName);
 
-  if (searchText) {
-    var startTimeFind = Date.now();
-    users = context.functions.execute(
-      "Private_SearchUsers",
-      searchText,
-      pagination
+  var findResult;
+  try {
+    // Get a value from the context (see "Values" tab)
+    // Update this to reflect your value's name.
+    var valueName = "value_name";
+    var value = context.values.get(valueName);
+
+    // Execute a FindOne in MongoDB 
+    findResult = await collection.findOne(
+      { owner_id: context.user.id, "fieldName": value, "argField": arg},
     );
-    console.log(`Total time Private_SearchUsers took::: `, (Date.now() - startTimeFind) / 1000);
-  } else {
-    var startTimeFind = Date.now();
-    users = context.functions.execute(
-      "Private_GetUserByEmailOrId",
-      {
-        userId,
-        userEmail,
-        callerType,
-      },
-      pagination
-    );
-    console.log(`Private_GetUserByEmailOrId  took::: `, (Date.now() - startTimeFind) / 1000);
+
+  } catch(err) {
+    console.log("Error occurred while executing findOne:", err.message);
+
+    return { error: err.message };
   }
-  return users;
-}
 
-// Function exported to App Services
-exports = SearchUsers;
+  // To call other named functions:
+  // var result = context.functions.execute("function_name", arg1, arg2);
 
-// export locally for use in unit test
-if (typeof module !== "undefined") {
-  module.exports = { SearchUsers };
-}
+  return { result: findResult };
+};
